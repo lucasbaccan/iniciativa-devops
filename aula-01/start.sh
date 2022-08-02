8d1c40cd38fd232d1dc70d5846f72d116c560851
Lucas Elias Baccan <lucas.baccan@matera.com>
2022-08-02 12:02:37 -0300

    Desafio

--- aula-01/start.sh
+++ aula-01/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker build -t aula-01 .
-docker run -it aula-01
+
+# docker build -t aula-01 Dockerfile.exemplo
+# docker run -it aula-01
+
+docker build -t desafio .
+docker run -it desafio
+
